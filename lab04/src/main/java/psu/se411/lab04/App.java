312da6f4b96381b13ad26f7719977634f8f8a42d
firase15 <222110757@psu.edu.sa>
2026-09-07 12:47:39 +0300

    lab04 done

---
+++ lab04/src/main/java/psu/se411/lab04/App.java
@@ -0,0 +1,22 @@
+package psu.se411.lab04;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            primaryStage.setTitle("My Project");
+            primaryStage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
